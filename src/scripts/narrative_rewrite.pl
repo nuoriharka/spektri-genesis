958f5d1b97e21be8122412049276a00746c512ea
Spektri-Genesis Node <lauri.rainio@outlook.com>
2026-01-17 06:40:35 +0200

    Create narrative_rewrite.pl

---
+++ src/scripts/narrative_rewrite.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+# MODULE: REALITY_REGEX_PARSER
+# DESC: The Swiss Army Chainsaw of Consciousness
+# "If you can't read this code, you aren't ready for Tuesday."
+
+use strict;
+use warnings;
+
+my $reality = "The patient is waiting in the hospital ward.";
+print ">> [PERL] OLD REALITY: $reality\n";
+
+# 1. APPLY THE 119% REGEX FILTER
+# s/pattern/replacement/global
+$reality =~ s/patient/Independent Architect/g;
+$reality =~ s/waiting/accelerating/g;
+$reality =~ s/hospital ward/Global Operations Center/g;
+$reality =~ s/\./ with 0ms latency./;
+
+# 2. OBFUSCATED LOGIC (Perl magic)
+# Tämä rivi näyttää kiroilulta, mutta se on validia koodia.
+my $chaos = "??!!@@##";
+if ($chaos =~ m/^\W+$/) {
+    print ">> [PERL] CHAOS DETECTED -> CONVERTING TO ENERGY.\n";
+}
+
+print ">> [PERL] NEW REALITY: $reality\n";
+# "Hups, I used regular expressions to edit the timeline." :DDDD
